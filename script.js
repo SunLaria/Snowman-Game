1f2e4c534fa03d69e8967176b8f8cfca6d9f6bcc
RandomG <sun4151@gmail.com>
2024-01-03 19:31:02 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
-WORD_API_URL="https://random-word-api.herokuapp.com/word"
-WORD_DEF_URL="https://api.api-ninjas.com/v1/dictionary?word="
-
 
 // get random word function
+WORD_API_URL="https://random-word-api.herokuapp.com/word"
+
 function getWordAPI(){
     const request = axios.get(WORD_API_URL);
     const data = request.then((response) => response.data[0])
@@ -12,11 +11,11 @@ function getWordAPI(){
 
 
 // get word defenition function
+WORD_DEF_URL="https://api.dictionaryapi.dev/api/v2/entries/en/"
+
 function getDefAPI(word){
-    const request = axios.get(WORD_DEF_URL+word,{
-        headers: { 'X-Api-Key': 'Os/BPM9azs+sDanCldsW2w==ajJ4ceH4z7VzU8TD'}
-    });
-    const data = request.then((response) => response.data)
+    const request = axios.get(WORD_DEF_URL+word);
+    const data = request.then((response) => response)
     return data
 }
 
@@ -25,30 +24,30 @@ function getDefAPI(word){
 function setDef(game_word){
     game_word.then(response => {
         getDefAPI(response).then(response => {
-            if (response.valid==true){
-                document.getElementById("word-defenition").innerHTML=response.definition
-            }else{
-                document.getElementById("word-defenition").innerHTML="No defenitions"
+            if (response.data.title=="No Definitions Found"){
+                document.getElementById("word-defenition").innerHTML="No Definitions Found"
+            } else{
+            document.getElementById("word-defenition").innerHTML=response.data[0].meanings[0].definitions.slice(0,2).map(item => item.definition).join("")
             }
         })
     })
 }
 
 
-// convert api given word into "_" list
-function get_hidden_word(game_word,array){
-    let result = [];
-        game_word.then((e) =>{
+// convert api given word into "_" list and update thewordingame
+function update_hidden_word(game_word,array){
+    game_word.then((e) =>{
+        let result = [];
         for (let i = 0; i < Array.from(e).length; i++) {
             letter = Array.from(e)[i]
             if(array.indexOf(letter) != -1){
-                console.log("contain");
                 result.push(letter);
             }else{
                 result.push("-");
-            }
-    }})
-    return result
+            }}
+        document.getElementById("hidden-word").innerHTML = result.join("    ");
+})
+    
 }
 
 
@@ -57,13 +56,11 @@ function startGame(){
     document.getElementById("menu").style.display="none";
     document.getElementById("main-game").style.display="block";
     let userGuess = new Array;
-    let hiddenWord = new Array
-    let tries = 0
+    let fails = 0
     let game_word = getWordAPI();
+    update_hidden_word(game_word,userGuess);
+    let compare = document.getElementById("hidden-word").innerHTML
     setDef(game_word)
-    const defenitionDiv = document.getElementById("word-defenition")
-
-
     const keyboardDiv = document.getElementById("keyboard-div");
     let letters = "abcdefghijklmnopqrstuvwxyz";
     for (let index = 0; index < letters.length; index++) {
@@ -71,17 +68,15 @@ function startGame(){
         button.innerHTML=letters[index];
         button.className="key-button";
         button.addEventListener("click",()=>{
-            if (tries>=7){
-                // end game function, WIP
+            userGuess.push(button.innerHTML);
+            update_hidden_word(game_word,userGuess);
+            console.log(compare,document.getElementById("hidden-word").innerHTML)
+            if (compare == document.getElementById("hidden-word").innerHTML){
+                fails+=1
             }
-            userGuess.push(button.innerHTML)
-            tries+=1
-            hiddenWord = get_hidden_word(game_word,userGuess);
-            document.getElementById("hidden-word").innerText = hiddenWord.join(" ");
             button.disabled=true;
-            // debug
-            console.log(document.getElementById("hidden-word").innerText) // why its empty
-            console.log({'Current Word':game_word,'Hidden Word':hiddenWord,'User Gusess':userGuess,'User Tries':tries});
+            compare = document.getElementById("hidden-word").innerHTML
+            console.log({'Current Word':game_word,'User Gusess':userGuess,'fails':fails});
             //
             
         })
